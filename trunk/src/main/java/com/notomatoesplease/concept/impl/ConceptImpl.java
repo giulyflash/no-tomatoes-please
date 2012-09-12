93f47e937c95a1c397f4e00f2a22f0b3994c9d62
david.schmiddi.86@gmail.com <david.schmiddi.86@gmail.com@d742f89c-e1ec-de1b-9482-eca22e286887>
2012-09-12 19:02:44 +0000

    structure update

--- trunk/src/main/java/com/notomatoesplease/concept/impl/ConceptImpl.java
+++
@@ -1,7 +0,0 @@
-package com.notomatoesplease.concept.impl;
-
-import com.notomatoesplease.concept.IConcept;
-
-public class ConceptImpl implements IConcept {
-
-}
--- trunk/src/main/java/com/notomatoesplease/persistence/impl/FilePersistenceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.notomatoesplease.persistence.impl;
-
-import com.notomatoesplease.persistence.IPersistence;
-
-public class FilePersistenceImpl implements IPersistence {
-
-}
