fc0c89920859dbdc1b27b890f480fb5fbcc790ea
Nico.Sorger <Nico.Sorger@gmail.com>
2012-09-19 20:08:54 +0000

    add sample jcurses test

--- src/main/java/com/notomatoesplease/app/App.java
+++ src/main/java/com/notomatoesplease/app/App.java
@@ -1,37 +1,90 @@
 package com.notomatoesplease.app;
 
 import java.util.List;
-import java.util.Random;
+
+import jcurses.event.ActionEvent;
+import jcurses.event.ActionListener;
+import jcurses.event.ItemEvent;
+import jcurses.event.ItemListener;
+import jcurses.event.ValueChangedEvent;
+import jcurses.event.ValueChangedListener;
+import jcurses.event.WindowEvent;
+import jcurses.event.WindowListener;
+import jcurses.system.CharColor;
+import jcurses.util.Message;
+import jcurses.widgets.Button;
+import jcurses.widgets.DefaultLayoutManager;
+import jcurses.widgets.Label;
+import jcurses.widgets.TextField;
+import jcurses.widgets.Widget;
+import jcurses.widgets.WidgetsConstants;
+import jcurses.widgets.Window;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.terminal.Terminal;
 import com.notomatoesplease.domain.Ingredient;
 import com.notomatoesplease.domain.Pizza;
 import com.notomatoesplease.util.GsonUtil;
 
-public class App {
+public class App extends Window implements ItemListener, ActionListener, ValueChangedListener, WindowListener,
+                WidgetsConstants {
 
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
+    private static App window = null;
+    private static TextField textfield = null;
+    private static Button button = null;
+
+    public App(final int width, final int height) {
+        super(width, height, true, "JCurses Test");
+    }
+
     public static void main(final String[] args) {
         LOG.info("Pizza zu JSON konvertiert: {}", GsonUtil.toJson(setUpExamplePizza()));
+        window = new App(30, 20);
+        window.init();
+    }
 
-        final Terminal t = TerminalFacade.createTextTerminal();
-        t.enterPrivateMode();
+    public void init() {
+        final DefaultLayoutManager mgr = new DefaultLayoutManager();
+        mgr.bindToContainer(window.getRootPanel());
+        mgr.addWidget(new Label("Hello World!", new CharColor(CharColor.WHITE, CharColor.GREEN)), 0, 0, 20, 10,
+                        WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
 
-        while (t.readInput() == null) {
-            t.applyBackgroundColor(randomColor());
-            t.applyForegroundColor(randomColor());
-            t.moveCursor(randomInt(0, t.getTerminalSize().getColumns()), randomInt(0, t.getTerminalSize().getRows()));
-            t.putCharacter((char) randomInt(65, 91));
-            t.moveCursor(0, 0);
+        textfield = new TextField(10);
+        mgr.addWidget(textfield, 0, 0, 20, 20, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+
+        button = new Button("Quit");
+        mgr.addWidget(button, 0, 0, 20, 30, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+
+        button.setShortCut('q');
+        button.addListener(this);
+        window.addListener(this);
+        window.show();
+    }
+
+    public void actionPerformed(final ActionEvent event) {
+        final Widget w = event.getSource();
+        if (w == button) {
+            new Message("HowTo", "You are about to quit", "OK").show();
+            window.close();
         }
+    }
 
-        t.exitPrivateMode();
+    public void stateChanged(final ItemEvent e) {
+    }
+
+    public void valueChanged(final ValueChangedEvent e) {
+    }
+
+    public void windowChanged(final WindowEvent event) {
+        if (event.getType() == WindowEvent.CLOSING) {
+            event.getSourceWindow().close();
+            // Toolkit.clearScreen(new CharColor(CharColor.WHITE,
+            // CharColor.BLACK));
+        }
     }
 
     private static Pizza setUpExamplePizza() {
@@ -69,15 +122,6 @@ public class App {
         pizza.setToppings(toppings);
 
         return pizza;
-    }
-
-    private static int randomInt(final int low, final int high) {
-        return (int) (Math.random() * (high - low) + low);
-    }
-
-    private static Terminal.Color randomColor() {
-        final int pick = new Random().nextInt(Terminal.Color.values().length);
-        return Terminal.Color.values()[pick];
     }
 
 }
