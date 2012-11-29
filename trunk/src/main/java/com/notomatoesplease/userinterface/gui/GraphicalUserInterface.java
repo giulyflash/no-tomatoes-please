e4cbb166047f132cd5cd252ead5d7ddc65b902a9
david.schmiddi.86@gmail.com <david.schmiddi.86@gmail.com@d742f89c-e1ec-de1b-9482-eca22e286887>
2012-11-29 18:12:19 +0000

    improved gui example

--- trunk/src/main/java/com/notomatoesplease/userinterface/gui/GraphicalUserInterface.java
+++ trunk/src/main/java/com/notomatoesplease/userinterface/gui/GraphicalUserInterface.java
@@ -1,5 +1,10 @@
 package com.notomatoesplease.userinterface.gui;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -7,6 +12,7 @@ import javax.swing.JTabbedPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.notomatoesplease.domain.Size;
 import com.notomatoesplease.logic.Logic;
 import com.notomatoesplease.userinterface.AbstractUserInterface;
 import com.notomatoesplease.userinterface.UserInterface;
@@ -24,17 +30,43 @@ public class GraphicalUserInterface extends AbstractUserInterface implements Use
 
     @Override
     public void run() {
-        JFrame mainFrame = new JFrame("Pizza Bäcker");
-        mainFrame.setSize(800,500);
+        // Window
+        List<Size> sizeList = getLogic().getSizes();
+
+        String[] sizeNames = new String[sizeList.size()];
+        int i = 0;
+
+        for (Size size : sizeList) {
+            sizeNames[i] = size.getName();
+            i++;
+        }
+
+        JComboBox sizeComboBox = new JComboBox(sizeNames);
+
         JPanel panelIngredients = new JPanel();
         JPanel panelPizza = new JPanel();
+        panelPizza.add(sizeComboBox);
 
         JTabbedPane tabs = new JTabbedPane(JTabbedPane .TOP);
         tabs.add("Zutaten bearbeiten", panelIngredients);
         tabs.add("Pizza erstellen", panelPizza);
 
+
+
+
+
+        JFrame mainFrame = new JFrame("Pizza Bäcker");
+        mainFrame.setSize(800,500);
         mainFrame.add(tabs);
         mainFrame.setVisible(true);
-    }
 
+        // Listener
+        mainFrame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(final WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+    }
 }
