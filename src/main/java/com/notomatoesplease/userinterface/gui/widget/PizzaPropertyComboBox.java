3d519999bae8ae3ab9cf74c9def0ebf5ff460bbc
david.schmiddi.86 <david.schmiddi.86@gmail.com>
2012-12-21 01:24:13 +0000

    working graphical interface, more documentation images

--- src/main/java/com/notomatoesplease/userinterface/gui/widget/PizzaPropertyComboBox.java
+++ src/main/java/com/notomatoesplease/userinterface/gui/widget/PizzaPropertyComboBox.java
@@ -1,5 +1,6 @@
 package com.notomatoesplease.userinterface.gui.widget;
 
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JComboBox;
@@ -8,7 +9,7 @@ import com.google.common.collect.Lists;
 import com.notomatoesplease.domain.PizzaProperty;
 
 @SuppressWarnings("serial")
-public final class PizzaComboBox<T extends PizzaProperty> extends JComboBox<String> {
+public final class PizzaPropertyComboBox<T extends PizzaProperty> extends JComboBox<String> {
 
     private final List<T> properties = Lists.newArrayList();
 
@@ -16,7 +17,7 @@ public final class PizzaComboBox<T extends PizzaProperty> extends JComboBox<Stri
      * JComboBox with a list of pizza properties
      * @param properties
      */
-    public PizzaComboBox(final List<T> properties) {
+    public PizzaPropertyComboBox(final List<T> properties) {
         super();
 
         for (final T property : properties) {
@@ -29,6 +30,7 @@ public final class PizzaComboBox<T extends PizzaProperty> extends JComboBox<Stri
      * @return the currently selected pizza property
      */
     public T getSelectedProperty() {
+        // TODO catch empty lists
         return properties.get(super.getSelectedIndex());
     }
 
@@ -36,6 +38,12 @@ public final class PizzaComboBox<T extends PizzaProperty> extends JComboBox<Stri
      * @param (re)sets the list of properties
      */
     public void setPropertyList(final List<T> properties) {
+        ActionListener[] al = super.getActionListeners();
+
+        for (ActionListener listener : al) {
+            super.removeActionListener(listener);
+        }
+
         this.properties.clear();
         super.removeAll();
 
@@ -43,5 +51,18 @@ public final class PizzaComboBox<T extends PizzaProperty> extends JComboBox<Stri
             this.properties.add(property);
             super.addItem(property.getName());
         }
+
+        for (ActionListener listener : al) {
+            super.addActionListener(listener);
+        }
+    }
+
+
+    /**
+     * @param property
+     */
+    public void addProperty(final T property) {
+        this.properties.add(property);
+        super.addItem(property.getName());
     }
 }
